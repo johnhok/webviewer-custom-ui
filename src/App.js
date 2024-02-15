e82429ea9417252fe3b6377077a8b87737381fc1
John Hok <hok@csdisco.com>
2024-02-15 14:17:25 -0500

    Update example

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useCallback } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "./App.css";
 
 const App = () => {
@@ -6,39 +6,34 @@ const App = () => {
 
   const viewer = useRef(null);
   const scrollView = useRef(null);
+  const timeoutRef = useRef(null);
 
   // if using a class, equivalent of componentDidMount
-  const loadViewer = useCallback(() => {
+  useEffect(() => {
     const Core = window.Core;
     Core.setWorkerPath("/webviewer");
 
-    const documentViewer = new Core.DocumentViewer();
+    let documentViewer = new Core.DocumentViewer();
     documentViewer.setScrollViewElement(scrollView.current);
     documentViewer.setViewerElement(viewer.current);
     documentViewer.loadDocument("/files/demo.pdf");
 
     documentViewer.addEventListener("documentLoaded", () => {
       console.log("document loaded");
-      setTimeout(async function () {
+      timeoutRef.current = setTimeout(async function () {
         await documentViewer.closeDocument();
         console.log("Document is closed");
         await documentViewer.dispose();
         console.log("Document is disposed");
         setIsVisible(false);
-      }, 3000);
+        timeoutRef.current = null;
+        documentViewer = null;
+      }, 10000);
     });
   }, []);
 
   return (
     <div className="App">
-      <button
-        onClick={() => {
-          setIsVisible(true);
-          loadViewer();
-        }}
-      >
-        Load viewer
-      </button>
       {isVisible ? (
         <div className="viewer">
           <div id="main-column">
