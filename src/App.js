6a76935c9dcd227a3b6419f5b4a772a689ec79cd
John Hok <hok@csdisco.com>
2024-02-15 14:25:23 -0500

    Repro in another way

--- src/App.js
+++ src/App.js
@@ -1,55 +1,56 @@
 import React, { useRef, useState, useEffect } from "react";
 import "./App.css";
 
-const App = () => {
-  const [isVisible, setIsVisible] = useState(true);
-
+const Internal = () => {
   const viewer = useRef(null);
   const scrollView = useRef(null);
   const timeoutRef = useRef(null);
 
   // if using a class, equivalent of componentDidMount
   useEffect(() => {
-    let Core = window.Core;
+    const Core = window.Core;
     Core.setWorkerPath("/webviewer");
 
-    let documentViewer = new Core.DocumentViewer();
+    const documentViewer = new Core.DocumentViewer();
     documentViewer.setScrollViewElement(scrollView.current);
     documentViewer.setViewerElement(viewer.current);
     documentViewer.loadDocument("/files/demo.pdf");
 
     documentViewer.addEventListener("documentLoaded", () => {
       console.log("document loaded");
-      timeoutRef.current = setTimeout(async function () {
-        await documentViewer.closeDocument();
-        console.log("Document is closed");
-        await documentViewer.dispose();
-        console.log("Document is disposed");
-        setIsVisible(false);
-        timeoutRef.current = null;
-        documentViewer = null;
-        Core = null;
-      }, 10000);
     });
+
+    return () => {
+      documentViewer.closeDocument();
+      console.log("Document is closed");
+      documentViewer.dispose();
+      console.log("Document is disposed");
+    };
   }, []);
 
   return (
     <div className="App">
-      {isVisible ? (
-        <div className="viewer">
-          <div id="main-column">
-            <div
-              className="flexbox-container"
-              id="scroll-view"
-              ref={scrollView}
-            >
-              <div id="viewer" ref={viewer}></div>
-            </div>
+      <div className="viewer">
+        <div id="main-column">
+          <div className="flexbox-container" id="scroll-view" ref={scrollView}>
+            <div id="viewer" ref={viewer}></div>
           </div>
         </div>
-      ) : null}
+      </div>
     </div>
   );
+};
+
+const App = () => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsVisible(false);
+    }, 10000);
+  });
+
+  return isVisible ? <Internal /> : null;
 };
 
 export default App;
